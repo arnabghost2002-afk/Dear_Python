ffc0c9054a11a7816c1074d85a21dd31889860d1
Arnab K. Ghosh <arnabghost2002@gmail.com>
2026-07-14 10:17:17 +0600

    Create codex4.py

---
+++ codex4.py
@@ -0,0 +1,9 @@
+#gonna do initials
+#mine is A B
+print('      AAA        BBBBBB')
+print('     A   A       B     B ')
+print('    A     A      B     B ')
+print('   A  A A  A     B B B B ')
+print('  A         A    B     B ')
+print(' A           A   B     B ')
+print('A             A  B B B B  ')
