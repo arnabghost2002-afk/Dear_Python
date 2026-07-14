22eea52af64d69892d509dbf494a5acc99b6b24b
Arnab K. Ghosh <arnabghost2002@gmail.com>
2026-07-14 12:50:47 +0600

    Create codex5.py

---
+++ codex5.py
@@ -0,0 +1,12 @@
+print('Wrote myself a letter')
+print('just a single qs')
+print ('why do fireflies have to die soon')
+print('is it because of the environment')
+print('or is it destined to be')
+print('dont know about that but i will give my best')
+print('i wont give up easily')
+print('i will try my best')
+print('')
+print('                        - Arnab K Ghosh')
+
+
