69e09783102577928d78d3284c2e9f2556034954
Arnab K. Ghosh <arnabghost2002@gmail.com>
2026-07-28 00:25:33 +0600

    Create Type_casting_broC0de.py

---
+++ Type_casting_broC0de.py
@@ -0,0 +1,22 @@
+name="arnab"
+roll=13
+cgpa=4.00
+student= True
+
+print(type(name))
+print(type(roll))
+print(type(cgpa))
+print(type(student))
+
+#type_casting
+
+#explicit
+cgpa=int(cgpa)
+print(cgpa)
+
+
+#implicit
+x=2
+y=2.0
+x= x/y
+print(x)
