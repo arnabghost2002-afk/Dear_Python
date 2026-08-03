10fb95772375547337ca662f77347b053ba65d11
Arnab K. Ghosh <arnabghost2002@gmail.com>
2026-08-03 12:32:23 +0600

    Create MadLibs_bro.py

---
+++ MadLibs_bro.py
@@ -0,0 +1,10 @@
+adjective=input("Enter an Adjective: ")
+noun=input("Enter a Noun: ")
+adjective2=input ("Enter an Adjective: ")
+verb=input("Enter a verb: ")
+adjective3=input("Enter an Adjective: ")
+
+print(f"\nToday i went to a {adjective} zoo")
+print(f"In an exihibit I saw a/an {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"If I  was {adjective3}"
