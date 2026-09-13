7d8381b4ec168bd188196ab6e14d1de8a5d466f2
Arnab K. Ghosh <arnabghost2002@gmail.com>
2026-09-13 23:47:39 +0600

    Create Format_specifier.py

---
+++ Format_specifier.py
@@ -0,0 +1,18 @@
+price1=3.1416
+price2=-334.1416
+price3=123334.1416
+
+
+print(f"price 1 is ${price1:.2f}")
+print(f"price 2 is ${price2:.2f}")
+print(f"price 3 is ${price3:.2f}")
+
+#SPaces
+print(f"price 1 is ${price1:10}")
+print(f"price 2 is ${price2:10}")
+print(f"price 3 is ${price3:10}")
+
+#lot of things
+print(f"price 1 is ${price1:+,.2f}")
+print(f"price 2 is ${price2:+,.2f}")
+print(f"price 3 is ${price3:+,.2f}")
